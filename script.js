42037fad58f34fb8c718016d0ee52d350d417c3d
Valerie Russell <vruss14@gmail.com>
2021-03-29 17:41:09 -0400

    responsive design and new rows created each time form is submitted

--- script.js
+++ script.js
@@ -14,3 +14,57 @@ $(function () {
     selectOtherMonths: true
   });
 });
+
+let table = document.getElementById("column-values")
+let addBtn = document.getElementById("add-proj");
+let form = document.getElementById("form-projects");
+
+
+addBtn.addEventListener("click", addProject)
+
+function addProject (event) {
+
+  event.preventDefault();
+
+  let projName = document.getElementById("proj-name").value.trim();
+  let projType = document.getElementById("types").value.trim();
+  let rate = document.getElementById("rate-value").value.trim();
+  let dueDate = document.getElementById("due-date").value.trim();
+
+
+  console.log("Listening :)");
+  console.log(projName);
+  console.log(projType);
+  console.log(rate);
+  console.log(dueDate);
+
+  form.reset();
+
+  addNewRow(projName, projType, rate, dueDate);
+}
+
+function addNewRow(projName, projType, rate, dueDate) {
+  let row = document.createElement("div");
+  table.appendChild(row);
+  row.setAttribute("class", "row row-cols-4");
+
+  let nameOfProj = document.createElement("div");
+  row.appendChild(nameOfProj);
+  nameOfProj.setAttribute("class", "col custom-col adjust-col");
+  nameOfProj.textContent = projName;
+
+  let typeOfProj = document.createElement("div");
+  row.appendChild(typeOfProj);
+  typeOfProj.setAttribute("class", "col custom-col adjust-col");
+  typeOfProj.textContent = projType;
+
+  let rateOfProj = document.createElement("div");
+  row.appendChild(rateOfProj);
+  rateOfProj.setAttribute("class", "col custom-col adjust-col");
+  rateOfProj.textContent = `$ ${rate}`;
+
+  let dateOfProj = document.createElement("div");
+  row.appendChild(dateOfProj);
+  dateOfProj.setAttribute("class", "col custom-col adjust-col");
+  dateOfProj.textContent = dueDate;
+}
